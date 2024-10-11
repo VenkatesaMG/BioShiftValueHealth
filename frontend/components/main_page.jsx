6db57a92e60426098b26c17f2d62f67ecb63aa97
VenkatesaMG <m.g.venkatesa@gmail.com>
2024-10-11 22:51:50 +0530

    Till Page 3

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -1,27 +1,66 @@
 import { IoIosMenu } from "react-icons/io"
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { CiSearch } from "react-icons/ci"
 import { Button } from '@mui/material'
 import "../src/index.css";
-import { IoIosArrowRoundForward } from "react-icons/io";
+import { IoIosArrowRoundForward, IoIosArrowRoundDown } from "react-icons/io"
 import Doctor from '../src/assets/Doctor.gif'
+import Image2 from '../src/assets/image2.gif'
+import { gsap } from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image3 from '../src/assets/AllinOne.gif'
+import SplitType from 'split-type'
 
 export default function mainPage() {
     const [displayNav, setDisplayNav] = useState(false)
     function handleDisplay() {
         setDisplayNav(prevState => !prevState)
-        const n = document.getElementById("nav");
-        if(n.classList.contains("hideClass")){
-            n.classList.remove("hideClass")
-        }else {
-            n.classList.add("hideClass")
-        }
-        console.log(n.classList)
+        // const n = document.getElementById("nav");
+        // if(n.classList.contains("hideClass")){
+        //     n.classList.remove("hideClass")
+        // }else {
+        //     n.classList.add("hideClass")
+        // }
+        // console.log(n.classList)
     }
+
+    gsap.registerPlugin(ScrollToPlugin)
+    
+    function handleGotoPage2(){
+        gsap.to(window, {duration: 1,
+            scrollTo:{y:".page2", offsetY:70}
+        })
+    }
+
+    function handleGotoPage3(){
+        gsap.to(window, {duration: 1,
+            scrollTo:{y:".page3", offsetY:70}
+        })
+
+        gsap.to("#all", {
+            scrollTo:{
+                trigger:'#all',
+                toggleActions: 'restart pause resume none'
+            },
+            scrollTrigger:{
+                trigger:'#all',
+                toggleActions: 'restart pause resume none'
+            },
+            y:100,
+            stagger:0.05,
+            delay:0.2,
+            duration: 1,
+        })
+    }
+        // const split = new SplitType(".outline-text", {
+        //     types:'lines, chars, words'
+        // })
+
     return (
         <div className='main-page-outer'>
-            <div className='nav-outer' id="nav">
-                {<div className='nav-bar'>
+            {displayNav && <div className='nav-outer' id="nav">
+                <div className='nav-bar'>
                     <div className='nav-bar-inner'>
                         <div className='nav-indicator'>
                         </div>
@@ -32,8 +71,8 @@ export default function mainPage() {
                             <div><a href="">Services</a></div>
                         </div>
                     </div>
-                </div>}
-            </div>
+                </div>
+            </div>}
             <div className="canvas">
                 <div className='menu-ribbon-div'>
                     <div className='menu-icon-div'>
@@ -51,7 +90,7 @@ export default function mainPage() {
                         </div>
                     </div>
                 </div> */}
-                <div className='right'>
+                <div className='page1'>
                     {/* <div className='content'>
                         <div>
                             <h2 style={{margin: 10, fontSize: "13px", fontFamily: "sans-serif", letterSpacing:'2px', width:'fit-content'}}>PURPOSE OF MYDNA</h2>
@@ -70,13 +109,64 @@ export default function mainPage() {
                                 <div>Analyze Medical</div>
                                 <div>Consultations</div>
                             </div>
-                            {/* <div className='hero-section-img'>
+                            <div className='hero-section-img'>
                                 <img src={Doctor} alt="" />
-                            </div> */}
+                            </div>
                         </div>
                         <div>
-                            <Button variant="outlined" id='how-it-works'>How it Works <IoIosArrowRoundForward id='right-arrow' /></Button>
+                            <Button variant="outlined" id='how-it-works' onClick={handleGotoPage2}>How it Works <IoIosArrowRoundDown id='right-arrow' /></Button>
                         </div>
+                    </div>
+                </div>
+                <div className='page2-outer'>
+                    <div className='page2-inter'>
+                        <div className='page2'>
+                            <div>
+                                <div className='page2-content'>
+                                    <div>
+                                        <h2 className='page2-h2'>How it Works</h2>
+                                    </div>
+                                    <div>
+                                        We automatically transcribe and analyze every doctor-patient interaction for better
+                                        documentation and care.
+                                    </div>
+                                    <div>
+                                        The transcribed text is analyzed for key medical insights and recommendations.
+                                    </div>
+                                    <div>
+                                        All transcriptions and reports are securely stored and easily accessible.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='page2-img'>
+                                <img src={Image2} alt="" />
+                            </div>
+                        </div>
+                        <div className='right-icon-page2'>
+                            <button className='down-icon-button' onClick={handleGotoPage3}><IoIosArrowRoundDown id='right-arrow-page2' /></button>
+                        </div>
+                    </div>
+                </div>
+                <div className='page3-outer'>
+                    <div className='page3'>
+                        <div>
+                            <div className='page3-content'>
+                                <div className='outline-text'>
+                                    <h2>All your Patient Data in One Place</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='page3-img'>
+                            <img src={Image3} alt="" />
+                        </div>
+                        <div className="page3-content">
+                            <div className='page3-text'>Track, store, and review medical records and reports at any time.</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='page4-outer'>
+                    <div className='page4'>
+                        
                     </div>
                 </div>
             </div>
